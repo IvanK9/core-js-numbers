df6b329ff67458016b0536f24fdbdcc5a0bb3eee
Ivan <ivankonovalov96@inbox.ru>
2023-11-19 02:02:56 +0500

    update core-js-number

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -258,8 +258,23 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  let x = 1;
+  let y = 1;
+  // eslint-disable-next-line no-plusplus
+  if (index === 0) {
+    return 0;
+  }
+  if (index === 1) {
+    return 1;
+  }
+  // eslint-disable-next-line no-plusplus
+  for (let i = 3; i <= index; i++) {
+    const z = x + y;
+    x = y;
+    y = z;
+  }
+  return y;
 }
 
 /**
@@ -273,8 +288,14 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let result = 0;
+  // eslint-disable-next-line no-plusplus
+  for (let i = 1; i <= n; i++) {
+    // eslint-disable-next-line no-const-assign
+    result += i;
+  }
+  return result;
 }
 
 /**
